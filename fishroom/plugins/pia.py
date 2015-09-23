4b89e6734a0c2cffaa24b3597da3ef7841917492
bigeagle <justin.w.xd@gmail.com>
2015-09-24 00:04:02 +0800

    update pia

--- fishroom/plugins/pia.py
+++ fishroom/plugins/pia.py
@@ -6,14 +6,15 @@ from ..command import command
 
 @command("pia", desc="Pia somebody", usage="pia [name]")
 def pia(cmd, *args, **kwargs):
+    _pia = "Pia!<(=ｏ ‵-′)ノ☆ "
     if len(args) == 0:
         # pia the bot
         msg = kwargs.get("msg", None)
         to = msg.sender if msg is not None else ""
-        return "Pia ☆ ～ %s" % to
+        return "%s %s" % (_pia, to)
     elif len(args) == 1:
-        return "Pia ☆ ～ %s" % args[0]
+        return "%s %s" % (_pia, args[0])
     elif len(args) > 1:
-        return "Too many persons to Pia"
+        return "Too many persons to %s" % _pia
 
 # vim: ts=4 sw=4 sts=4 expandtab
