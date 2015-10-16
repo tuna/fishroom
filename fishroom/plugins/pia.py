958a8c149ad9b8f01c9655bbf1c5b25a43eaa449
bigeagle <justin.w.xd@gmail.com>
2015-10-16 14:46:41 +0800

    mua command

--- fishroom/plugins/pia.py
+++ fishroom/plugins/pia.py
@@ -23,4 +23,22 @@ def pia(cmd, *args, **kwargs):
     elif len(args) > 1:
         return "Too many persons to %s" % _pia
 
+
+@command("mua", desc="mua somebody", usage="mua [name]")
+def mua(cmd, *args, **kwargs):
+    _mua = "💋 Mua!  "
+    room = kwargs.get('room', "ALL")
+    if rlimiter.check(room, cmd, period=15, count=2) is False:
+        return "Rate limited to " + _mua
+
+    if len(args) == 0:
+        # pia the bot
+        msg = kwargs.get("msg", None)
+        sender = msg.sender if msg is not None else ""
+        return "%s %s himself/herself" % (sender, _mua)
+    elif len(args) == 1:
+        return "%s %s" % (_mua, args[0])
+    elif len(args) > 1:
+        return "Too many persons to %s" % _mua
+
 # vim: ts=4 sw=4 sts=4 expandtab
